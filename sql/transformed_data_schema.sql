509382e0aa858cac149c082d36a6f667c8b9cc62
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-11-10 15:17:35 -0500

    📝 Docs: final notebook of database uploading and query writing process

---
+++ sql/transformed_data_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS transformed_data (
+"Happiness_Rank" SMALLINT,
+"Country" TEXT,
+"Happiness_Score" FLOAT,
+"GDP_per_capita" FLOAT,
+"Freedom" FLOAT,
+"Generosity" FLOAT,
+"Perceptions_of_corruption" FLOAT,
+"Year" SMALLINT
+);
