7dc5efbdced8f802d00e80f578e1e361263e9c28
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-11-15 15:21:58 -0500

    🐛 Fix: corrected model application and migration to Random Forest

--- sql/transformed_data_schema.sql
+++ sql/transformed_data_schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS transformed_data (
 "Country" TEXT,
 "Happiness_Score" FLOAT,
 "GDP_per_capita" FLOAT,
+"Social_support" FLOAT,
+"Health_(Life_Expectancy)" FLOAT,
 "Freedom" FLOAT,
 "Generosity" FLOAT,
 "Perceptions_of_corruption" FLOAT,
